926b1c252ffa8c874578dbf18283deff36152b41
sleshJdev <slesh.eugene93@gmail.com>
2017-10-27 12:34:35 +0300

    the jwt token creation was implemented

--- api/routes/login.js
+++ api/routes/login.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
+const appConfig = require('../config/app.config');
 const User = require('../persistence/mongo/model/user');
 const AuthError = require('../error/auth-error');
 
@@ -12,14 +14,24 @@ router.post('/', (req, res, next) => {
         next(error);
       } else if (!user) {
         next(new AuthError(`User with email ${req.body.email} not found`));
-      } else if (user.password === req.body.password) {
-        user.password = null;
-        res.send({
-          key: Math.random().toString(),
-          user: user
-        });
+      } else if (user.password !== req.body.password) {
+        next(new AuthError(`Provided user password(${req.body.password}) is incorrect`));
       } else {
-        res.sendStatus(401);
+        jwt.sign(
+          {
+            name: user.name,
+            email: user.email,
+            phone: user.phone,
+            address: user.address
+          },
+          appConfig.secretKey,
+          (error, token) => {
+            if (error) {
+              next(error);
+            } else {
+              res.send(token);
+            }
+          });
       }
     });
 });
