03680968c471c6114041004b08969669610e3a82
sleshJdev <slesh.eugene93@gmail.com>
2017-10-27 17:19:08 +0300

    the mongo deprecation warning was fixed(http://mongoosejs.com/docs/connections.html#use-mongo-client)

--- api/persistence/mongo/index.js
+++ api/persistence/mongo/index.js
@@ -4,7 +4,9 @@ const appConfig = require('../../config/app.config');
 const mongoConfig = appConfig.persistence.mongo;
 
 module.exports = (done, error) => {
-  mongoose.connect(mongoConfig.url);
+  mongoose.connect(mongoConfig.url, {
+    useMongoClient: true
+  });
   mongoose.connection.on('connected', done);
   mongoose.connection.on('close', error);
 };
