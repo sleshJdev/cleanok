8e337ee3fb1f98011a7e6c08f811453b598da597
sleshJdev <slesh.eugene93@gmail.com>
2017-10-30 15:04:46 +0300

    basis users crud operations were realised

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,29 +1,57 @@
 const express = require('express');
+const HttpError = require('../error/http-error');
 const User = require('../persistence/mongo/model/user');
 
-const router = express.Router();
-
-router.get('/:id', (req, res, next) => {
-  User.findById(
-    req.params.id,
-    (error, user) => {
+module.exports = express.Router()
+  .get('/', (req, res, next) => {
+    User
+      .find({})
+      .select('-password')
+      .exec((error, users) => {
+        if (error) {
+          next(error);
+        } else {
+          res.send(users);
+        }
+      });
+  })
+  .get('/:id', (req, res, next) => {
+    User
+      .findById(req.params.id)
+      .select('-password')
+      .exec((error, user) => {
+        if (error) {
+          next(error);
+        } else {
+          res.send(user);
+        }
+      });
+  })
+  .post('/', (req, res, next) => {
+    const user = new User(req.body);
+    user.save((error, user) => {
       if (error) {
         next(error);
       } else {
-        res.send(user);
+        res.send({_id: user._id});
       }
     });
-});
-
-router.post('/', (req, res, next) => {
-  const user = new User(req.body);
-  user.save((error, user) => {
-    if (error) {
-      next(error);
-    } else {
-      res.end();
-    }
+  })
+  .delete('/:id', (req, res, next) => {
+    User.findById(
+      req.params.id,
+      (error, user) => {
+        if (error) {
+          return next(error);
+        }
+        if (!user) {
+          return next(new HttpError(`User(id=${req.params.id} not found`, 404));
+        }
+        user.remove((error) => {
+          if (error) {
+            return next(error);
+          }
+          res.end();
+        });
+      });
   });
-});
-
-module.exports = router;
