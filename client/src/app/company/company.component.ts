b09360464646cfcfbdf843185c76c9fc27229f63
sleshJdev <slesh.eugene93@gmail.com>
2017-11-01 18:20:33 +0300

    css + grid experimenting

---
+++ client/src/app/company/company.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+import {UserStatus} from '../enum/user-status.enum';
+
+@Component({
+  selector: 'app-company',
+  templateUrl: './company.component.html',
+  styleUrls: ['./company.component.css']
+})
+export class CompanyComponent implements OnInit {
+
+
+
+  constructor() {
+  }
+
+  ngOnInit() {
+
+  }
+
+
+}
---
+++ client/src/app/company/list/companies.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import {UserStatus} from '../../enum/user-status.enum';
+
+@Component({
+  selector: 'app-companies',
+  templateUrl: './companies.component.html',
+  styleUrls: ['./companies.component.css']
+})
+export class CompaniesComponent implements OnInit {
+
+  protected companies: any[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.companies = [{
+      title: 'Big Brothers',
+      email: 'bigbrothers@mail.com',
+      status: UserStatus[UserStatus.active],
+      comment: null
+    }, {
+      title: 'Electronic Airlines',
+      email: 'eairlines@mail.com',
+      status: UserStatus[UserStatus.active],
+      comment: null
+    }, {
+      title: 'Martin Cookies',
+      email: 'martincookies@mail.com',
+      status: UserStatus[UserStatus.block],
+      comment: 'Terms of User violation'
+    }]
+  }
+
+  isBlock(company) {
+    return company.status === UserStatus[UserStatus.block];
+  }
+}
